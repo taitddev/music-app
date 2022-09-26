ee69412faf530e368182ee80b915158653e6530e
taitddev <taitd.dev@gmail.com>
2022-09-27 00:36:23 +0700

    initial project

---
+++ src/components/SongCard.jsx
@@ -0,0 +1,13 @@
+import { useDispatch } from 'react-redux';
+
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group"></div>
+    </div>
+  );
+};
+
+export default SongCard;
